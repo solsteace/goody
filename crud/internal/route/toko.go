259102041b9c284cf1a155fdf18622fadaff170f
Allen Rasheed <solsteace@proton.me>
2025-08-27 18:38:17 +0700

    fix: missing authentication checker

--- crud/internal/route/toko.go
+++ crud/internal/route/toko.go
@@ -3,15 +3,19 @@ package route
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/solsteace/goody/crud/internal/controller"
+	"github.com/solsteace/goody/crud/internal/util/middleware"
 )
 
 func UseToko(
 	parent *fiber.Router,
 	controller *controller.Toko,
+	authChecker middleware.AuthChecker,
 ) {
 	toko := (*parent).Group("/toko")
 	toko.Get("/my", controller.GetSelf)
 	toko.Get("/:id", controller.GetById)
 	toko.Get("/", controller.GetMany)
+
+	toko.Use(authChecker.Handle)
 	toko.Put("/:id", controller.UpdateById)
 }
